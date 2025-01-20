e016c4cc310fd230d7088a9504a7840f82743e1f
Code-Vibe-Studios <codevibestudios@gmail.com>
2025-01-20 21:03:00 +0530

    day6 js intro

---
+++ FrontEnd/day6/app.js
@@ -0,0 +1,14 @@
+// alert("Welcome to JS Intro class");
+// let username = prompt("Enter your name");
+// alert("Hello " + username);
+// console.log("Hello " + username);
+
+
+let age = prompt("Enter your age");
+// if(age >= 18){
+//     alert("You are eligible to vote");
+// }else{
+//     alert("You are not eligible to vote");
+// }
+
+let result = (age >= 18) ? alert("You are eligible to vote") : alert("You are not eligible to vote");
